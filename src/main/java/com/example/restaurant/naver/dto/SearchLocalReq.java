e227a261f01e82091ef5c475d366a1d51f2cbce0
inderby <youk8253@gmail.com>
2023-01-28 15:19:18 +0900

    implement NaverClient.localSearch function(and add function to return queryParams to MultiValueMap)

--- src/main/java/com/example/restaurant/naver/dto/SearchLocalReq.java
+++ src/main/java/com/example/restaurant/naver/dto/SearchLocalReq.java
@@ -3,6 +3,8 @@ package com.example.restaurant.naver.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Data
 @NoArgsConstructor
@@ -17,5 +19,13 @@ public class SearchLocalReq {
 
     private String sort = "random";
 
+    public MultiValueMap<String, String> toMultiValueMap(){
+        var map = new LinkedMultiValueMap<String, String>();
 
+        map.add("query", query);
+        map.add("display", String.valueOf(display));
+        map.add("start", String.valueOf(start));
+        map.add("sort", sort);
+        return map;
+    }
 }
